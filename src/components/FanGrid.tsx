91507e7828e9e278b1087c5acfd1098a4d803b78
Luca.Mueller <Luca.Mueller@em-lap-251.lan>
2021-03-30 10:20:34 +0200

    add fan & fan grid

--- src/components/FanGrid.tsx
+++ src/components/FanGrid.tsx
@@ -1,40 +1,17 @@
 import React from 'react';
-import logo from '../logo.svg';
-import './App.css';
 import Button from "./Button";
+import './FanGrid.css';
 
-class FanGrid extends React.Component {
-  render() {
-    return <div>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-    </div>;
-  }
-}
-
-function App() {
+function FanGrid() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <FanGrid/>
-      </header>
-    </div>
+      <div className="fan__grid__wrapper">
+        <Button></Button>
+        <Button></Button>
+        <Button></Button>
+        <Button></Button>
+        <Button></Button>
+      </div>
   );
 }
 
-export default App;
+export default FanGrid;
